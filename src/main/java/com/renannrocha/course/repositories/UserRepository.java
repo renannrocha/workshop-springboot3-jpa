d570f69772c22203133f35992b008010bb8c0011
renannrocha <renanrocha8897@gmail.com>
2024-01-29 19:10:33 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/renannrocha/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.renannrocha.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.renannrocha.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
