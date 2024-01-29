7930ba5ed91f86d6e7db98f20c02ee2bc9b4fd71
renannrocha <renanrocha8897@gmail.com>
2024-01-29 19:37:24 -0300

    Service layer, component registration

---
+++ src/main/java/com/renannrocha/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.renannrocha.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renannrocha.course.entities.User;
+import com.renannrocha.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	// retornar todos os usuários do banco de dados
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	// retornar por id
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
