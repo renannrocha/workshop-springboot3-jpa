c6a7d618736cfc9a74f1009009483cc4fa43d307
renannrocha <renanrocha8897@gmail.com>
2024-01-30 21:56:52 -0300

    Order, Instant, ISO 8601 Pt2

---
+++ src/main/java/com/renannrocha/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.renannrocha.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.renannrocha.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
