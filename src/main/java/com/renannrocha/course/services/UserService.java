dbfb9b6349b8cedf0cc42677ddfd09e9414d4773
renannrocha <renanrocha8897@gmail.com>
2024-02-01 14:20:12 -0300

    Exception handling - update

--- src/main/java/com/renannrocha/course/services/UserService.java
+++ src/main/java/com/renannrocha/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.renannrocha.course.repositories.UserRepository;
 import com.renannrocha.course.services.exceptions.DatabaseException;
 import com.renannrocha.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -50,10 +52,14 @@ public class UserService {
 	
 	//atualizar
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-		
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
