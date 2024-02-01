60c1ace04d80a8baa692dcd5f184554016986a95
renannrocha <renanrocha8897@gmail.com>
2024-02-01 12:45:27 -0300

    User update

--- src/main/java/com/renannrocha/course/services/UserService.java
+++ src/main/java/com/renannrocha/course/services/UserService.java
@@ -36,4 +36,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	//atualizar
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
