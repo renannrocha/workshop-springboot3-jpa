7a293cd679334c8ed1e11871b636ab1c275358f9
renannrocha <renanrocha8897@gmail.com>
2024-02-01 14:12:41 -0300

    Exception handling - delete

--- src/main/java/com/renannrocha/course/services/UserService.java
+++ src/main/java/com/renannrocha/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.renannrocha.course.entities.User;
 import com.renannrocha.course.repositories.UserRepository;
+import com.renannrocha.course.services.exceptions.DatabaseException;
 import com.renannrocha.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,7 +37,15 @@ public class UserService {
 	
 	// deletar
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} 
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	//atualizar
